4052d3824581f9f933b791515728c6ce553090a3
Ragib Shahariar Ayon <ragibsayon@gmail.com>
2023-11-11 00:09:51 +0600

    feat Add lint-staged

--- server/src/middlewares/notFound.middleware.ts
+++ server/src/middlewares/notFound.middleware.ts
@@ -1,7 +1,7 @@
 import { type RequestHandler } from 'express'
 import { NotFoundError } from '../errors'
 
-const notFound: RequestHandler = async () => {
+const notFound: RequestHandler = async (req, res) => {
   throw new NotFoundError('Route does not exist.')
 }
 
